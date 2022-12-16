75f38cd9e99b0d2f70412560fdcfcf383d4a8669
Mujtaba Abdul Qadir <68260826+mujtaba-abdul-qadir-cse@users.noreply.github.com>
2022-12-16 16:18:05 +0600

    Add files via upload
    A program that displays the flag of Bangladesh by using MATLAB's built-in graph plotting feature.

---
+++ bangladesh_flag.m
@@ -0,0 +1,5 @@
+rectangle('Position',[0 0 10 6], 'FaceColor',["#006a4e"])
+axis([0 10 0 6])
+circle = [2.5 1 4 4];
+rectangle('Position',circle,'Curvature',[1 1], 'FaceColor',["#f42a41"])
+axis equal
